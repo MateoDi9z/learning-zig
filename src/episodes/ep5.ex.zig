a84a45dbe0525f371588ba4dc1ccf98f13e6c313
Mateo <dzmateozd@gmail.com>
2024-03-09 11:38:32 -0300

    Ej. 5 & Ej. 6

---
+++ src/episodes/ep5.ex.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.log.info("All your codebase are belong to us.", .{});
+
+    // uint or null
+    var t: ?u8 = null;
+
+    std.debug.print("t: {?}", .{t});
+    t = 42;
+    std.debug.print("t: {?}", .{t});
+
+    // Throw Error if null
+    var the_byte = t.?;
+
+    // if null 12 else t
+    the_byte = t orelse 12;
+
+    if (t) {
+        std.debug.print("It's {}!\n", .{t});
+    } else if (false) {
+        std.debug.print("What?n {}!\n", .{t});
+    } else {
+        std.debug.print("What?n {}!\n", .{t});
+    }
+
+    // capture value of t exp in b
+    if (t) |b| {
+        std.debug.print("b is {}!\n", .{b});
+    } else {
+        //
+    }
+
+    // ignore capture
+    if (true) |_| {
+        //
+    }
+
+    // ternary op
+    t = if (true) |b| b else 0;
+    t = true orelse 0; // shorter and same
+
+    // Multi line and multi condition
+    the_byte = if (t != null and t.? == 42)
+        42 * 2
+    else
+        0;
+}
---
+++ src/episodes/ep6.ex.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.log.info("All your codebase are belong to us.", .{});
+
+    // A block defines a new scope
+    {
+        const a = 5;
+        std.debug.print("a = {}\n", .{a});
+    }
+
+    // a is not defined in <- this scope
+
+    // blocks are expressions with a label that return a value
+    const x: u8 = block: {
+        const y: u8 = 13;
+        const z = 42;
+        break :block y + z;
+    };
+    std.debug.print("x = {}\n", .{x});
+
+    switch (x) {
+        0...20 => std.debug.print("A", .{}),
+        21, 22, 23 => std.debug.print("B", .{}),
+        40...60 => |a| std.debug.print("a: {}", .{a}),
+        77 => {
+            // block
+            std.debug.print("A", .{});
+        },
+        // block has to be comptime known
+        block: {
+            const a = 100;
+            const b = -50;
+            break :block a + b;
+        } => std.debug.print("a + b"),
+
+        else => std.debug.print("Else...", .{}),
+    }
+
+    const answer: u8 = switch (x) {
+        0...10 => 1,
+        20...50 => 2,
+        else => 3,
+    };
+    std.debug.print("Answer: {}", .{answer});
+}
