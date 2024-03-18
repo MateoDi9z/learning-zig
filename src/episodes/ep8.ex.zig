7d2ef909758ed21e21ca7f8488be8bc6d28bbd62
Mateo <dzmateozd@gmail.com>
2024-03-17 23:22:13 -0300

    Ep. 8 fixes and review

--- src/episodes/ep8.ex.zig
+++ src/episodes/ep8.ex.zig
@@ -4,12 +4,13 @@ pub fn main() !void {
     // A slice with comptime known bounds results in a pointer to an array
     var array = [_]u8{ 0, 1, 2, 3, 4, 5 };
     const array_ptr = array[0..array.len];
-    std.debug.print("\n\narray_ptr type: {}\n", .{@TypeOf(array_ptr)});
+    std.debug.print("\n\narray: {any}\n", .{array});
+    std.debug.print("array_ptr type: {}\n", .{@TypeOf(array_ptr)});
 
     std.debug.print("\n", .{});
 
     // force runtime 0
-    const zero: usize = 0;
+    var zero: usize = 0;
 
     // slice by using slicing syntax
     const a_slice: []u8 = &array;
@@ -21,32 +22,43 @@ pub fn main() !void {
     std.debug.print("a_slice.len: {}\n", .{@TypeOf(a_slice.len)});
     std.debug.print("\n", .{});
 
-    const b_slice = array[zero..];
-    //b_slice.ptr += 2; // Pointer arithmetic on multi-item pointer
-    // b_slice.len -= 2;
-
-    std.debug.print("b_slice: {any}\n", .{b_slice});
+    // Slice with runtime-known bounds.
+    var b_slice = array[zero..];
+    b_slice.ptr += 2; // Pointer arithmetic on multi-item pointer
+    std.debug.print("b_slice with moved ptr: {any}\n", .{b_slice});
+    b_slice.len -= 2;
+    std.debug.print("b_slice shrinked: {any}\n", .{b_slice});
     std.debug.print("\n", .{});
 
     // slicing slices
-    const c_slice = array_ptr[zero..3];
+    std.debug.print("array: {any}\n", .{array});
+    const c_slice: []const u8 = a_slice[zero..3]; // coercing var slice into const slice
     std.debug.print("c_slice ([zero..3]): {any}\n", .{c_slice});
 
     // slices have bound checking
-    // c_slice[10] = 2; //! Error - Panic
+    // c_slice[10] = 2; // ! Error - Panic
     std.debug.print("\n", .{});
 
     const d_slice = array_ptr[zero..2];
-    std.debug.print("d_slice: {any}\n", .{d_slice});
+    std.debug.print("d_slice: {any}\n\n", .{d_slice});
 
     // Sentinel terminated slice
     array[4] = 0;
     const e_slice: [:0]u8 = array[0..4 :0];
     std.debug.print("e_slice[e_slice.len]: {}\n", .{e_slice[e_slice.len]});
-    std.debug.print("e_slice: {any}\n", .{e_slice});
+    std.debug.print("e_slice: {any}, e_slice.len: {}\n", .{ e_slice, e_slice.len });
 
-    const start: usize = 2;
-    const length: usize = 4;
+    std.debug.print("\n", .{});
+
+    var start: usize = 2;
+    var length: usize = 4;
+
+    std.debug.print("array {any}\n", .{array});
     const f_slice = array[start..][0..length];
-    std.debug.print("f_slice: {any}\n", .{f_slice});
+    std.debug.print("start: {}, length: {}\n", .{ start, length });
+    std.debug.print("f_slice = array[start..][0..length]: {any}\n", .{f_slice});
+
+    zero = 1;
+    start = 3;
+    length = 5;
 }
